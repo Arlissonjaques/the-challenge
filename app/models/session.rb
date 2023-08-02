f1be6261a8b499456cc60512998546f809f85bb5
Arlisson Jaques <arlissonjaques@gmail.com>
2023-08-02 17:53:45 -0300

    includes session template helper methods

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,21 @@
 class Session < ApplicationRecord
   belongs_to :user
+
+  def self.search(user_id, token)
+    Session.find_by(token: token, status: true, user_id: user_id)
+  end
+
+  def is_late?
+    if (last_used_at + TOKEN_LIFETIME) >= Time.now
+      false
+    else
+      update(status: false)
+
+      true
+    end
+  end
+
+  def used
+    update(last_used_at: Time.now)
+  end
 end
