21ad94ebc7b7124d97e841de87ce1d1d27aa34b0
Arlisson Jaques <arlissonjaques@gmail.com>
2023-08-03 17:55:10 -0300

    includes new validations, relations and methods in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,50 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :sessions, dependent: :destroy
+  has_many :user_verifications, dependent: :destroy
 
-  def confirmed?
+  before_save :downcase_email!
+
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true,
+            uniqueness: {
+              case_sensitive: false
+            },
+            format: {
+              with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/,
+              message: I18n.t("errors.models.user.format_email")
+            }
+  validates :password,
+            format: {
+              with: /\A(?=.*).{8,72}\z/,
+              message: I18n.t("errors.models.user.format_password")
+            }, if: :password_required?
+
+  after_create :send_confirm_email
+
+  def email_confirmed?
     !email_confirmed_at.nil?
+  end
+
+  def confirm
+    update(email_confirmed_at: Time.now)
+  end
+
+  def send_confirm_email
+    unless email_confirmed?
+      verification = UserVerification.create(user_id: id, verify_type: :confirm_email)
+      # TODO: send confirmation email
+    end
+  end
+
+  def send_reset_password_email
+    if email_confirmed?
+      verification = UserVerification.create(user_id: id, verify_type: :reset_email)
+      # TODO: send reset password email
+    end
+  end
+
+  def downcase_email!
+    email&.donwcase!
   end
 end
--- app/views/users/_self.json.jbuilder
+++ app/views/users/_self.json.jbuilder
@@ -1,2 +1,2 @@
 json.extract! user, :id, :first_name, :last_name, :email, :created_at
-json.confirmed user.confirmed?
+json.confirmed user.email_confirmed?
