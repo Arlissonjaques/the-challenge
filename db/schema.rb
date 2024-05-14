08a5338e501c959dae4f248346304008026984b2
Arlisson Jaques <arlissonjaques@gmail.com>
2024-05-13 23:40:17 -0300

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_13_123803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.string "comment"
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "last_used_at"
+    t.boolean "status", default: true, null: false
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["last_used_at"], name: "index_sessions_on_last_used_at"
+    t.index ["status"], name: "index_sessions_on_status"
+    t.index ["token"], name: "index_sessions_on_token", unique: true
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "user_verifications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "status", default: "pending"
+    t.string "token"
+    t.string "verify_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["status"], name: "index_user_verifications_on_status"
+    t.index ["token"], name: "index_user_verifications_on_token", unique: true
+    t.index ["user_id"], name: "index_user_verifications_on_user_id"
+    t.index ["verify_type"], name: "index_user_verifications_on_verify_type"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "email_confirmed_at"
+    t.integer "role", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email_confirmed_at"], name: "index_users_on_email_confirmed_at"
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "sessions", "users"
+  add_foreign_key "user_verifications", "users"
 end
